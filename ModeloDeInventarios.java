6cd6ef1b0f5344082f253b1570c029c75d060a48
Edgar Licona Cazares <edgarliconacaz@gmail.com>
2025-11-14 01:55:34 +0000

    Commit

---
+++ ModeloDeInventarios.java
@@ -0,0 +1,46 @@
+package modelodeinventarios;
+
+import java.util.Scanner;
+
+import javafx.scene.control.Menu;
+
+public class ModeloDeInventarios {
+
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        int opcion = 0;
+        int k, d, y, to, diasSobreTo, dias, le, pr, tcu;
+        double h;
+
+        do {
+            System.out.println("Menu");
+            System.out.println("1. Modelo de inventarios clasico");
+            System.out.println("2. Modelo de inventarios con descuento");
+            System.out.println("3. Salir");
+            System.out.println("Opcion: ");
+            opcion = lector.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.println("MODELO DE INVENTARIOS CLASICO");
+                    System.out.println("Teclea el costo de pedido");
+                    k = lector.nextInt();
+                    System.out.println("Teclea el valor de la demanda");
+                    d = lector.nextInt();
+                    System.out.println("Teclea el valor de cada unidad");
+                    h = lector.nextDouble();
+
+                    y = (int) Math.sqrt((2 * k * d) / h);
+
+                    System.out.println("Valor de y* " + y);
+                    
+                case 3:
+                    System.out.println("Saliendo");
+                    break;
+
+                default:
+
+                    break;
+            }
+        } while (opcion != 3);
+    }
+}
